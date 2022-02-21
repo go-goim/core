5e18143980ba2c8ff8d3fc19d99fa2f09a6319f6
yusank <yusankurban@gmail.com>
2022-02-21 22:30:20 +0800

    fix: update conn

--- pkg/conn/ws_manage.go
+++ pkg/conn/ws_manage.go
@@ -1,25 +1,33 @@
 package conn
 
 import (
+	"encoding/json"
+	"net"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 	messagev1 "github.com/yusank/goim/api/message/v1"
 )
 
 type WsConn struct {
-	websocket.Conn
-	mu sync.Mutex
-	// todo use websocket.Conn ping func instead of ping channel
-	pingChan <-chan struct{}
+	*websocket.Conn
+	key      string
+	mu       sync.Mutex
+	pingChan chan struct{}
 }
 
 func (w *WsConn) PushMessage(message *messagev1.PushMessage) error {
-	panic("implement me")
+	b, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	return w.WriteMessage(websocket.TextMessage, b)
 }
 
 func (w *WsConn) Key() string {
-	panic("implement me")
+	return w.key
 }
 
 func (w *WsConn) Ping() <-chan struct{} {
@@ -31,4 +39,27 @@ func (w *WsConn) Ping() <-chan struct{} {
 	ch := w.pingChan
 	w.mu.Unlock()
 	return ch
+}
+
+// closedChan is a reusable closed channel.
+var closedChan = make(chan struct{})
+
+func (w *WsConn) pingFunc(message string) error {
+	err := w.WriteControl(websocket.PongMessage, []byte(message), time.Now().Add(time.Second))
+	if err == nil {
+		if w.pingChan == nil {
+			w.pingChan = closedChan
+		} else {
+			close(w.pingChan)
+		}
+
+		return nil
+	}
+
+	if err == websocket.ErrCloseSent {
+		return nil
+	} else if e, ok := err.(net.Error); ok && e.Temporary() {
+		return nil
+	}
+	return err
 }
