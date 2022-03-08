205d726a40fac4961eb1c69114a1d3904cf7fbe1
yusank <yusankurban@gmail.com>
2022-03-08 19:03:06 +0800

    fix: add consul but not finished

---
+++ pkg/registry/consul/registry.go
@@ -0,0 +1,135 @@
+package consul
+
+import (
+	"context"
+	"sync/atomic"
+
+	"github.com/go-kratos/kratos/v2/registry"
+
+	"github.com/hashicorp/consul/api"
+)
+
+var (
+	_ registry.Registrar = &Registry{}
+	_ registry.Discovery = &Registry{}
+)
+
+// Option is consul registry option.
+type Option func(*Registry)
+
+// WithHealthCheck with registry health check option.
+func WithHealthCheck(enable bool) Option {
+	return func(o *Registry) {
+		o.enableHealthCheck = enable
+	}
+}
+
+// WithHeartbeat enable or disable heartbeat
+func WithHeartbeat(enable bool) Option {
+	return func(o *Registry) {
+		if o.cli != nil {
+			o.cli.heartbeat = enable
+		}
+	}
+}
+
+// WithServiceResolver with endpoint function option.
+func WithServiceResolver(fn ServiceResolver) Option {
+	return func(o *Registry) {
+		if o.cli != nil {
+			o.cli.resolver = fn
+		}
+	}
+}
+
+// WithHealthCheckInterval with healthcheck interval in seconds.
+func WithHealthCheckInterval(interval int) Option {
+	return func(o *Registry) {
+		if o.cli != nil {
+			o.cli.healthcheckInterval = interval
+		}
+	}
+}
+
+// Config is consul registry config
+type Config struct {
+	*api.Config
+}
+
+// Registry is consul registry
+type Registry struct {
+	cli               *Client
+	enableHealthCheck bool
+}
+
+// New creates consul registry
+func New(apiClient *api.Client, opts ...Option) *Registry {
+	r := &Registry{
+		cli:               NewClient(apiClient),
+		enableHealthCheck: true,
+	}
+	for _, o := range opts {
+		o(r)
+	}
+	return r
+}
+
+// Register register service
+func (r *Registry) Register(ctx context.Context, svc *registry.ServiceInstance) error {
+	return r.cli.Register(ctx, svc, r.enableHealthCheck)
+}
+
+// Deregister deregister service
+func (r *Registry) Deregister(ctx context.Context, svc *registry.ServiceInstance) error {
+	return r.cli.Deregister(ctx, svc.ID)
+}
+
+// GetService return service by name
+func (r *Registry) GetService(ctx context.Context, name string) (services []*registry.ServiceInstance, err error) {
+	return r.cli.Service(ctx, name, false)
+}
+
+// ListServices return service list.
+func (r *Registry) ListServices(ctx context.Context) (allServices []*registry.ServiceInstance, err error) {
+	return r.cli.ListServices(ctx)
+
+}
+
+// Watch resolve service by name
+func (r *Registry) Watch(ctx context.Context, name string) (registry.Watcher, error) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	set, ok := r.registry[name]
+	if !ok {
+		set = &serviceSet{
+			watcher:     make(map[*watcher]struct{}),
+			services:    &atomic.Value{},
+			serviceName: name,
+		}
+		r.registry[name] = set
+	}
+
+	// 初始化watcher
+	w := &watcher{
+		event: make(chan struct{}, 1),
+	}
+	w.ctx, w.cancel = context.WithCancel(context.Background())
+	w.set = set
+	set.lock.Lock()
+	set.watcher[w] = struct{}{}
+	set.lock.Unlock()
+	ss, _ := set.services.Load().([]*registry.ServiceInstance)
+	if len(ss) > 0 {
+		// If the service has a value, it needs to be pushed to the watcher,
+		// otherwise the initial data may be blocked forever during the watch.
+		w.event <- struct{}{}
+	}
+
+	if !ok {
+		err := r.resolve(set)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return w, nil
+}
