1087d64ff2907cfd2544aedd26d12ee11301c86f
yusank <yusankurban@gmail.com>
2022-04-06 12:12:32 +0800

    fix: start clean up log #31

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,3 +1,10 @@
 package log
 
+import "github.com/go-kratos/kratos/v2/log"
+
 // define log api and implement
+
+func init() {
+	// TODO: add any other log implement.
+	log.SetLogger(log.DefaultLogger)
+}
