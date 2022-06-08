cccf7fe150c9c280b31a7a30c61ff7725d70ca46
yusank <yusankurban@gmail.com>
2022-06-08 11:15:43 +0800

    fix: use channel to control ws msg

--- pkg/conn/ws/pool.go
+++ pkg/conn/ws/pool.go
@@ -1,18 +1,18 @@
-package pool
+package ws
 
 import "sync"
 
 var dp = newNamedPool()
 
-func Add(c Conn) {
+func addToPool(c *WebsocketConn) {
 	dp.add(c)
 }
 
-func Get(key string) Conn {
+func Get(key string) *WebsocketConn {
 	return dp.get(key)
 }
 
-func LoadAllConn() chan Conn {
+func LoadAllConn() chan *WebsocketConn {
 	return dp.loadAllConns()
 }
 
@@ -34,9 +34,9 @@ func newNamedPool() *namedPool {
 	return p
 }
 
-func (p *namedPool) add(c Conn) {
+func (p *namedPool) add(c *WebsocketConn) {
 	select {
-	case <-c.Done():
+	case <-c.ctx.Done():
 		return
 	default:
 		if c.Err() != nil {
@@ -61,14 +61,14 @@ func (p *namedPool) add(c Conn) {
 	p.m[c.Key()] = i
 }
 
-func (p *namedPool) get(key string) Conn {
+func (p *namedPool) get(key string) *WebsocketConn {
 	p.RLock()
 	i, ok := p.m[key]
 	p.RUnlock()
 
 	if ok {
 		select {
-		case <-i.c.Done():
+		case <-i.c.ctx.Done():
 			i.stop()
 		default:
 			if i.c.Err() != nil {
@@ -82,11 +82,11 @@ func (p *namedPool) get(key string) Conn {
 	return nil
 }
 
-func (p *namedPool) loadAllConns() chan Conn {
+func (p *namedPool) loadAllConns() chan *WebsocketConn {
 	p.Lock()
 	defer p.Unlock()
 
-	ch := make(chan Conn, len(p.m))
+	ch := make(chan *WebsocketConn, len(p.m))
 	for _, i := range p.m {
 		ch <- i.c
 	}
