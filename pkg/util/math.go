289f725f861e46bb7ac3f15c73dc4778ded5cc9a
yusank <yusankurban@gmail.com>
2022-06-13 17:37:28 +0800

    fix: upgrade go version

--- pkg/util/math.go
+++ pkg/util/math.go
@@ -1,8 +1,13 @@
 package util
 
-func Min(a, b int) int {
+type Integer interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+func Min[T Integer](a, b T) T {
 	if a < b {
 		return a
 	}
+
 	return b
 }
