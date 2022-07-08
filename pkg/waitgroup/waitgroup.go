cf2429b56f06db8b34ee5b3af5b97dc5f0e57f58
yusank <yusankurban@gmail.com>
2022-07-08 18:37:08 +0800

    fix: update wg

--- pkg/waitgroup/waitgroup.go
+++ pkg/waitgroup/waitgroup.go
@@ -1,21 +1,30 @@
 package waitgroup
 
 import (
+	"context"
 	"sync"
 
+	"go.uber.org/atomic"
+
 	"github.com/go-goim/core/pkg/goroutine"
+	"github.com/go-goim/core/pkg/graceful"
 )
 
 type WaitGroup struct {
-	wg *sync.WaitGroup
-	ch chan struct{}
+	wg   *sync.WaitGroup
+	ch   chan struct{}
+	done *atomic.Bool
 }
 
 func NewWaitGroup(size int) *WaitGroup {
-	return &WaitGroup{
-		wg: new(sync.WaitGroup),
-		ch: make(chan struct{}, size),
+	wg := &WaitGroup{
+		wg:   new(sync.WaitGroup),
+		ch:   make(chan struct{}, size),
+		done: atomic.NewBool(false),
 	}
+
+	graceful.Register(wg.waitWhenShutdown)
+	return wg
 }
 
 func (wg *WaitGroup) Add(f func()) {
@@ -30,6 +39,26 @@ func (wg *WaitGroup) Add(f func()) {
 
 func (wg *WaitGroup) Wait() {
 	wg.wg.Wait()
+	wg.done.Store(true)
+}
+
+func (wg *WaitGroup) waitWhenShutdown(ctx context.Context) error {
+	if wg.done.Load() {
+		return nil
+	}
+
+	done := make(chan struct{})
+	go func() {
+		wg.wg.Wait()
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-done:
+		return nil
+	}
 }
 
 func (wg *WaitGroup) Done() {
